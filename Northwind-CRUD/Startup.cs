2cc8b15d51ed833793beadd0ad1d9a2fa0d8198f
Nick Chi <as880074@gmail.com>
2019-07-09 19:20:59 +0800

    加入專案檔案。

---
+++ Northwind-CRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Northwind_CRUD.Startup))]
+namespace Northwind_CRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
